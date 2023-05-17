c9afe91f8d723ebab1814f829c7647e499d6a117
Alcantn1 <nalcantara92@gmail.com>
2023-05-16 22:06:11 -0700

    added to dayjs

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const forecast = document.querySelector('.forecast');
 const APIkey = "8e429daa5b2c1b82a6e187f2f61c7f05"
-const searchHistory = [];
+// const searchHistory = [];
 const city ='sacramento';
 
 function getApi(city, APIkey) {
@@ -57,14 +57,13 @@ searchBtn.addEventListener('click', function(event) {
 });
 
 
-// forecast days
+// forecast days 
 const today = dayjs();
-const forecastDays = [today, today.add(1, 'day'), today.add(2, 'day'), today.add(3, 'day'), today.add(4, 'day')];
+const forecastDays = [
+    today.format(`yyyy-mm-dd`),
+    today.add(1, 'day').format(`yyyy-mm-dd`),
+    today.add(2, 'day').format(`yyyy-mm-dd`),
+     today.add(3, 'day').format(`yyyy-mm-dd`),
+     today.add(4, 'day').format(`yyyy-mm-dd`),
+]
 console.log(forecastDays);
-
-
-
-
-
-
-
