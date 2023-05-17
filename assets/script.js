cb4ef4809c4223444b2bc1726ffbbacb0be3160d
Alcantn1 <nalcantara92@gmail.com>
2023-05-16 23:33:01 -0700

    fixed code and added comments

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
-const forecast = document.querySelector('.forecast');
+// openweathermap Api key
 const APIkey = "8e429daa5b2c1b82a6e187f2f61c7f05"
 
+// retrieves current weather data from the Api for city searched
 function getApi(city) {
     const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
     fetch(queryURL)
@@ -12,6 +13,7 @@ function getApi(city) {
             console.log(data);
         });
 }
+// retrieves forecast data from api for city searched
 function getForecast(city) {
     const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkey;
     fetch(queryURL)
@@ -19,14 +21,16 @@ function getForecast(city) {
             return response.json();
         })
         .then(function(data) {
+            displayForecast(data);
             console.log(data);
         });
 }
-
+// displays current weather
 function displayWeather(data) {
 const forecastContainer = document.querySelector('.forecastContainer');
 forecastContainer.innerHTML = "";
 
+// creates and displays windspeed, temperature(in kelvin) and humidity
 const cityName = document.createElement('h3');
 cityName.textContent = data.name;
 
@@ -39,18 +43,18 @@ humidity.textContent = `humidity: ${data.main.humidity}%`;
 const windSpeed = document.createElement('p');
 windSpeed.textContent = `windSpeed: ${data.wind.speed}m/s`;
 
+// appends elements to forecastContainer
 forecastContainer.appendChild(cityName);
 forecastContainer.appendChild(temperature);
 forecastContainer.appendChild(humidity);
 forecastContainer.appendChild(windSpeed);
-
-
 }
-
+// displays forecast in forecastContainer
 function displayForecast(data) {
     const forecastContainer = document.querySelector('.forecastContainer');
     forecastContainer.innerHTML = ""; 
 
+// goes through forecast for the next 5 days
     for (let i = 0; i < 5; i++) {
         const forecastDay = document.createElement('div');
         forecastDay.classList.add('forecastDay');
@@ -80,7 +84,7 @@ function displayForecast(data) {
 const searchBtn = document.getElementById('searchBtn');
 const searchInput = document.querySelector('.form-control');
 
-
+// event listener for search button
 searchBtn.addEventListener('click', function(event) {
     event.preventDefault();
     const city = searchInput.value;
@@ -89,7 +93,7 @@ searchBtn.addEventListener('click', function(event) {
 });
 
 
-// forecast days (doesnt show year)
+// displays forecast dates
 const today = dayjs();
 const forecastDays = [
     today.format('YYYY-MM-DD'),
